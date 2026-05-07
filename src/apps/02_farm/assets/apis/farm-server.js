7345ed18b7f02ef1420677a7eb3f9107173a5251
조흥재 연구원 <heung@ubisam.com>
2026-05-07 16:57:07 +0900

    20260507 farm-server.js 구현

---
+++ src/apps/02_farm/assets/apis/farm-server.js
@@ -0,0 +1,408 @@
+import $base from "@/plugins/apis";
+import $userinfo from "@@/assets/stores/userinfo.js";
+
+const name = "[/assets/apis/farm-server.js]";
+
+const $server = {
+
+  api: {
+    execute(optionsBuilder){
+      return $base.meta
+        .env("VITE_FARM_SERVER", "VITE_OAUTH2_SERVER")
+        .then(optionsBuilder)
+        .then((e) => {
+          return $base.api.execute(e);
+        })
+        .then((e) => {
+          return $base.api.then(e);
+        })
+        .catch((e) => {
+          throw $base.api.catch(e);
+        });
+    },
+    url(env, path){
+      let paths = (path != null) ? path : "";
+      return `${env["VITE_FARM_SERVER"]}${paths}`;
+    },
+    token(env) {
+      let flag = env["VITE_OAUTH2_SERVER"] != undefined;
+      let token = flag ? $userinfo.computed.token.get() : undefined;
+      return token;
+    },
+    headers(env, headers){
+      let token = $server.api.token(env);
+      return $base.api.headers(headers, token);
+    },
+    params(env, params){
+      let token = $server.api.token(env);
+      return $base.api.params(params, token);
+    },
+    query(env, query){
+      let token = $server.api.token(env);
+      return $base.api.query(query, token);
+    },
+    pageable(data){
+      return $base.api.pageable(data);
+    }
+  },
+  ///////////////////////////////////////////////////////
+  oauth2: {
+    available(){
+      return $base.meta.env("VITE_OAUTH2_SERVER")
+        .then(e => {
+          let uri = e["VITE_OAUTH2_SERVER"];
+          if(uri == undefined){
+            return false;
+          }else{
+            return uri.startsWith("http");
+          }
+        })
+    },
+    userinfo(role){
+      return $server.api.execute((e) => ({
+        method: "GET",
+        url: $server.api.url(e, "/oauth2/userinfo"),
+        headers: $server.api.headers(e, {}),
+      })).then(r => {
+        if(role == undefined){
+          return r;
+        }
+        if(r.authorities == undefined){
+          return r;
+        }
+        r[role] = r.authorities.findIndex(e => e == role) > -1;
+        return r;
+      });
+    },
+  },
+  /////////////////////////////////////////////////////////////
+  farms: {
+    search(data, params){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/farms/search"),
+          headers: $server.api.headers(e, {}),
+          params: $server.api.pageable(params),
+          data: data,
+        }));
+    },
+    create(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/farms"),
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    read(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+    update(data){
+      return $server.api
+        .execute((e) => ({
+          method: "PUT",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    delete(data){
+      return $server.api
+        .execute((e) => ({
+          method: "DELETE",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+  },
+  ///////////////////////////////////////////////////////
+  farmDevices: {
+    search(data, params){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/farmDevices/search"),
+          headers: $server.api.headers(e, {}),
+          params: $server.api.pageable(params),
+          data: data,
+        }));
+    },
+    create(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/farmDevices"),
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    read(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+    update(data){
+      return $server.api
+        .execute((e) => ({
+          method: "PUT",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    delete(data){
+      return $server.api
+        .execute((e) => ({
+          method: "DELETE",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+  },
+  ///////////////////////////////////////////////////////
+  farmDeviceTypes: {
+    search(data, params){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/farmDeviceTypes/search"),
+          headers: $server.api.headers(e, {}),
+          params: $server.api.pageable(params),
+          data: data,
+        }));
+    },
+    create(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/farmDeviceTypes"),
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    read(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+    update(data){
+      return $server.api
+        .execute((e) => ({
+          method: "PUT",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    delete(data){
+      return $server.api
+        .execute((e) => ({
+          method: "DELETE",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+  },
+  ///////////////////////////////////////////////////////
+  farmPlants: {
+    search(data, params){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/farmPlants/search"),
+          headers: $server.api.headers(e, {}),
+          params: $server.api.pageable(params),
+          data: data,
+        }));
+    },
+    create(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/farmPlants"),
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    read(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+    update(data){
+      return $server.api
+        .execute((e) => ({
+          method: "PUT",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    delete(data){
+      return $server.api
+        .execute((e) => ({
+          method: "DELETE",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+  },
+  ///////////////////////////////////////////////////////
+  accounts: {
+    search(data, params){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/accounts/search"),
+          headers: $server.api.headers(e, {}),
+          params: $server.api.pageable(params),
+          data: data,
+        }));
+    },
+    create(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/accounts"),
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    read(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+    update(data){
+      return $server.api
+        .execute((e) => ({
+          method: "PUT",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    delete(data){
+      return $server.api
+        .execute((e) => ({
+          method: "DELETE",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+  },
+  ///////////////////////////////////////////////////////
+  devices: {
+    search(data, params){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/devices/search"),
+          headers: $server.api.headers(e, {}),
+          params: $server.api.pageable(params),
+          data: data,
+        }));
+    },
+    create(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/devices"),
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    read(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+    update(data){
+      return $server.api
+        .execute((e) => ({
+          method: "PUT",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    delete(data){
+      return $server.api
+        .execute((e) => ({
+          method: "DELETE",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+  },
+  ///////////////////////////////////////////////////////
+  sessions: {
+    search(data, params){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/sessions/search"),
+          headers: $server.api.headers(e, {}),
+          params: $server.api.pageable(params),
+          data: data,
+        }));
+    },
+    create(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: $server.api.url(e, "/api/sessions"),
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    read(data){
+      return $server.api
+        .execute((e) => ({
+          method: "POST",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+    update(data){
+      return $server.api
+        .execute((e) => ({
+          method: "PUT",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+          data: data,
+        }));
+    },
+    delete(data){
+      return $server.api
+        .execute((e) => ({
+          method: "DELETE",
+          url: data._links.self.href,
+          headers: $server.api.headers(e, {}),
+        }));
+    },
+  },
+};
+export default $server;
